8133e4db66dffe76bd45ca00cd0a65495026dbb9
Andrew Nagy <anagy@clearlyip.com>
2022-09-23 22:22:18 +0000

    fix readme

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function Home() {
-  return <FontAwesomeIcon icon="bars" fixedWidth color="black" />;
+  return <>
+    {/* Throws Errors */}
+    <FontAwesomeIcon icon="bars" fixedWidth color="black" />
+    {/* Works */}
+    <FontAwesomeIcon icon={faBars} fixedWidth color="black" />
+  </>;
 }
